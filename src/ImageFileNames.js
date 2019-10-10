76ff788ce17a4b14d41fb7422558bb7b3f22613d
Jason Allen Richard <Slayermonster0@gmail.com>
2019-10-09 22:38:45 -0700

    complete project

---
+++ src/ImageFileNames.js
@@ -0,0 +1,3 @@
+let ImageFileNames = ["alchemist.png", "ant.png", "bart.png", "bee.png", "boy.png", "girl.png", "homer.png", "lisa.png", "man.png", "panther.png", "plane.png", "pony.png", "robot.png", "sunshine.png", "pilot.png", "yoga.png"];
+
+export default ImageFileNames;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>Clicky Game © 2018 Copyright:&nbsp;<a href="https://github.com/muntasir2165" target="_blank" rel="noopener noreferrer">Muntasir Biojid</a></span>
+  </footer>
+);
+
+export default Footer;
